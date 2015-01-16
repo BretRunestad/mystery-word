028dec03df2cea2ac1686594de40e7b40e217329
Bret Runestad <bret.runestad@gmail.com>
2015-01-15 22:15:09 -0500

    added text functions

--- mystery_word.py
+++ mystery_word.py
@@ -22,5 +22,26 @@ def starting_text(string_length):
     print("I have chosen a word at random. It has {} letters.".format(string_length))
     print(word_interface(string_length))
 
+def guess_text(counter_int):
+    return print("Make a guess! You have {} left.".format(counter_int))
+
+def success_text():
+    print("Good guess!")
+
+def failure_text():
+    print("Sorry. You now have one less guess.")
+
+def get_input():
+    input("> ")
+
 def guess_checker(a_guess):
-    return True
+    a_guess = a_guess.upper()
+    if a_guess in chooser():
+        return True
+    else:
+        counter -= 1
+        return False
+
+chosen_word = chooser(all_words)
+string_length = counter(chosen_word)
+blank_dashes = word_interface(string_length)
