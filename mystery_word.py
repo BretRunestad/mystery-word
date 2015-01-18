316770b19d752fdb353b831e7d75bd25fb6464c3
Bret Runestad <bret.runestad@gmail.com>
2015-01-17 22:05:51 -0500

    Working on getting win-check to work

--- mystery_word.py
+++ mystery_word.py
@@ -14,13 +14,12 @@ def set_chosen_word_string(chosen_word):
     chosen_word_string = chosen_word.upper()
     return chosen_word_string
 
-def set_chosen_word_list(chosen_word):
-    chosen_word_upper = chosen_word.upper()
-    chosen_word_list = list(chosen_word_upper)
+def set_chosen_word_list(chosen_word_string):
+    chosen_word_list = list(chosen_word_string[:-1])
     return chosen_word_list
 
 def length_checker(a_string):
-    string_length = len(a_string)
+    string_length = len(a_string[:-1])
     return string_length
 
 def starting_text_a():
@@ -38,6 +37,17 @@ def guess_text(counter):
     print(a)
     return a
 
+def repeat_guess_check(capsletter, guess_list):
+    if capsletter in guess_list:
+        print ("""You've already guessed that letter.  Try another.""")
+        return "guess again"
+    else:
+        return "pass"
+
+def append_guess_list(guess, guess_list):
+    guess_list.append(guess)
+    return guess_list
+
 def success_text():
     yay = "Good guess!"
     print(yay)
@@ -51,44 +61,91 @@ def failure_text():
 def get_guess():
     letter = input("> ")
     capsletter = letter.upper()
-    guess_list.append(capsletter)
+    return capsletter
 
 def list_visualizer(chosen_word_list, guess_list):
     """"Takes the list form of the word you're trying to guess and checks
     it against the list of guesses to create a string visualization """
-    for index, letter in enumerate(chosen_word_list):
+    usage_list = chosen_word_list[:]
+    for index, letter in enumerate(usage_list):
         if letter not in guess_list:
-            chosen_word_list[index] = "_"
-            visual_string = ' '.join(chosen_word_list)
+            usage_list[index] = "_"
+            visual_string = ' '.join(usage_list)
+    print(visual_string)
     return visual_string
 
 def guess_checker(a_guess, chosen_word_string, counter):
     a_guess = a_guess.upper()
     if a_guess in chosen_word_string:
+        success_text()
         return counter
     else:
-        counter -= 1
+        counter = counter - 1
+        failure_text()
         return counter
 
 def win_check(chosen_word_list, guess_list):
-    for word in chosen_word_list:
-        if word in guess_list:
-            return True
-        else:
-            return False
-
-def lose_check(counter):
-    if counter > 0:
+    if all(item in guess_list for item in chosen_word_list):
         return True
     else:
         return False
+
+def lose_check(counter):
+    if counter > 0:
+        return False
+    else:
+        return True
 
 def victory_text(chosen_word_string):
     yay = "You win! The word was {}!".format(chosen_word_string)
     print(yay)
     return yay
 
-def failure_text(chosen_word_string):
+def loss_text(chosen_word_string):
     nay = "Sorry! You lose. The word was {}.".format(chosen_word_string)
     print(nay)
     return nay
+
+if __name__ == "__main__":
+    chosen_word = chooser(word_pull())
+    #Setting global variables for the program:
+    chosen_word_string = set_chosen_word_string(chosen_word)
+    chosen_word_list = set_chosen_word_list(chosen_word_string)
+    print(chosen_word_list)
+    chosen_word_length = length_checker(chosen_word)
+    cwl = chosen_word_list[:]
+
+    #print(chosen_word_list)
+    #print(chosen_word_string)
+
+    counter = 8
+    guess_list = []
+
+    starting_text_a()
+    starting_text_b(chosen_word_length)
+    list_visualizer(chosen_word_list, guess_list)
+
+    while counter > 0:
+        guess_text(counter)
+        capsletter = get_guess()
+        while repeat_guess_check(capsletter, guess_list) == "guess again":
+            capsletter = get_guess()
+        guess_list = append_guess_list(capsletter, guess_list)
+        #print(chosen_word_string)
+        #print(chosen_word_list)
+        #print(guess_list)
+        list_visualizer(chosen_word_list, guess_list)
+        counter = guess_checker(capsletter, chosen_word_string, counter)
+        #print(counter)
+
+        #print(chosen_word_list)
+        #print(guess_list)
+
+
+        x = win_check(chosen_word_list, guess_list)
+        #if x == True:
+            #victory_text(chosen_word_string)
+            #quit()
+
+    else:
+        loss_text(chosen_word_string)
--- test_file.py
+++ test_file.py
@@ -6,17 +6,24 @@ def test_chooser():
     assert mw.chooser(lista) in lista
 
 def test_length_checker():
-    assert mw.length_checker("alligator") == 9
+    assert mw.length_checker("alligator\n") == 9
 
 def test_guess_checker():
     assert mw.guess_checker('A', "APPLE", 8) == 8
     assert mw.guess_checker('B', "APPLE", 8) == 7
 
+def test_append_guess_list():
+    guess = 'B'
+    glist = ['D', 'C']
+    assert mw.append_guess_list(guess, glist) == ['D', 'C', 'B']
+
 def test_list_visualizer():
     lista = ['a', 'p', 'p', 'l', 'e']
     listb = ['p', 'q', 'r', 's', 't']
+    listc = ['a', 'p', 'e']
     assert type(mw.list_visualizer(lista, listb)) == str
     assert mw.list_visualizer(lista, listb) == '_ p p _ _'
+    assert mw.list_visualizer(lista, listc) == 'a p p _ e'
     assert mw.list_visualizer(lista, []) == '_ _ _ _ _'
 
 def test_set_chosen_word_string():
@@ -25,7 +32,7 @@ def test_set_chosen_word_string():
     assert mw.set_chosen_word_string(word) == "APPLE"
 
 def test_set_chosen_word_list():
-    word = 'apPle'
+    word = 'APPLE\n'
     assert type(mw.set_chosen_word_list(word)) == list
     assert mw.set_chosen_word_list(word) == ['A', 'P', 'P', 'L', 'E']
 
@@ -34,6 +41,14 @@ def test_starting_text_b():
 
 def test_guess_text():
     assert mw.guess_text(8) == "Make a guess! You have 8 left."
+
+def test_repeat_guess_check():
+    guessa = "A"
+    guessb = "B"
+    guess_list = ['B', 'C', 'D']
+
+    assert mw.repeat_guess_check(guessa, guess_list) == "pass"
+    assert mw.repeat_guess_check(guessb, guess_list) == "guess again"
 
 def test_win_check():
     lista = ['A', 'P', 'P', 'L', 'E']
@@ -45,11 +60,11 @@ def test_win_check():
 def test_lose_check():
     counter1 = 1
     counter2 = 0
-    assert mw.lose_check(counter1) == True
-    assert mw.lose_check(counter2) == False
+    assert mw.lose_check(counter1) == False
+    assert mw.lose_check(counter2) == True
 
 def test_victory_text():
     assert mw.victory_text("APPLE") == "You win! The word was APPLE!"
 
-def test_failure_text():
-    assert mw.failure_text("APPLE") == "Sorry! You lose. The word was APPLE."
+def test_loss_text():
+    assert mw.loss_text("APPLE") == "Sorry! You lose. The word was APPLE."
