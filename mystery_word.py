5bee6b91ab3bf93c1e5f0195838c49e61a71bb8d
Bret Runestad <bret.runestad@gmail.com>
2015-01-18 22:02:55 -0500

    working program with all 3 hard mode requirements

--- mystery_word.py
+++ mystery_word.py
@@ -1,16 +1,19 @@
 
 from random import choice
 
+
 def word_pull():
     """Pulls a list of words from the computer's dictionary"""
     with open("/usr/share/dict/words") as file:
         all_words = file.readlines()
     return all_words
 
+
 def chooser(a_list):
     """Chooses one word from a list of words"""
     chosen_word = choice(a_list)
     return chosen_word
+
 
 def set_chosen_word_string(chosen_word):
     """Provides a clean, uppper-cased string"""
@@ -18,28 +21,60 @@ def set_chosen_word_string(chosen_word):
     chosen_word_string = cleaned_word.upper()
     return chosen_word_string
 
+
 def set_chosen_word_list(chosen_word_string):
     """Creates list form of chosen_word_string"""
     chosen_word_list = list(chosen_word_string[:])
     return chosen_word_list
+
 
 def length_checker(a_string):
     """Determines the length of a string"""
     string_length = len(a_string[:-1])
     return string_length
 
+
+def easy_sorter(a_list):
+    """Sorts the list of all words into words of 4-6 letters"""
+    easy_list = [x for x in a_list if len(x) > 4 and len(x) < 8]
+    return easy_list
+
+
+def med_sorter(a_list):
+    """Sorts the list of all words into words of 6-10 letters"""
+    med_list = [x for x in a_list if len(x) > 6 and len(x) < 12]
+    return med_list
+
+
+def hard_sorter(a_list):
+    """Sorts the list of all words into words of 10+ letters"""
+    hard_list = [x for x in a_list if len(x) > 10]
+    return hard_list
+
+
 def starting_text_a():
     """Prints first sentence of starting text"""
-    a= "Let the Mystery Word game begin!"
+    a = "Let the Mystery Word game begin!"
     print(a)
     return(a)
 
-def starting_text_b(string_length):
+
+def starting_text_b():
+    print(
+        "Choose: \nE for Easy Mode, \nM for Medium Mode, \nH for Hard Mode")
+    game_mode = input("> ")
+    game_mode = game_mode.upper()
+    return game_mode
+
+
+def starting_text_c(string_length):
     """Prints second sentence of starting text, reveals how
     many letters are in the word"""
-    b = "I have chosen a word at random. It has {} letters.".format(string_length)
+    b = """I have chosen a word at random. It has {} letters.""".format(
+        string_length)
     print(b)
     return(b)
+
 
 def guess_text(counter):
     """Prompts the user to make a guess, informs how
@@ -48,22 +83,24 @@ def guess_text(counter):
     print(a)
     return a
 
+
 def repeat_guess_check(capsletter, guess_list):
     "Checks to see if the guessed letter has already been guessed"
     if capsletter in guess_list:
-        print ("""You've already guessed that letter.  Try another.""")
+        print("""You've already guessed that letter.  Try another.""")
         return "guess again"
     else:
         return "pass"
+
 
 def error_checking(capsletter):
     """Checks to make sure that the guess is a) not more than one
     letter, and b) a standard letter of the alphabet"""
     if len(capsletter) > 1:
-        print ("Please guess only one letter.")
+        print("Please guess only one letter.")
         return "not ok"
     elif capsletter not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        print ("Please guess a letter.")
+        print("Please guess a letter.")
         return "not ok"
     else:
         return "ok"
@@ -74,11 +111,13 @@ def append_guess_list(guess, guess_list):
     guess_list.append(guess)
     return guess_list
 
+
 def success_text():
     """Prints successful guess text"""
     yay = "Good guess!\n"
     print(yay)
     return yay
+
 
 def failure_text():
     """Prints unsuccessful guess text."""
@@ -86,11 +125,13 @@ def failure_text():
     print(nay)
     return nay
 
+
 def get_guess():
     """Takes an input from the user and capitalizes it"""
     letter = input("> ")
     capsletter = letter.upper()
     return capsletter
+
 
 def list_visualizer(chosen_word_list, guess_list):
     """"Takes the list form of the word you're trying to guess and checks
@@ -104,6 +145,7 @@ def list_visualizer(chosen_word_list, guess_list):
     print(visual_string)
     return visual_string
 
+
 def guess_checker(a_guess, chosen_word_string, counter):
     """Checks to see whether the guess is part of the chosen word,
     and lowers the counter by one for incorrect guesses"""
@@ -116,12 +158,14 @@ def guess_checker(a_guess, chosen_word_string, counter):
         failure_text()
         return counter
 
+
 def win_check(chosen_word_list, guess_list):
     """Tests to see whether all of the letters in the word have been guessed"""
     if all(item in guess_list for item in chosen_word_list):
         return True
     else:
         return False
+
 
 def lose_check(counter):
     """Tests to see whether the user has run our of guesses"""
@@ -130,17 +174,20 @@ def lose_check(counter):
     else:
         return True
 
+
 def victory_text(chosen_word_string):
     """Prints the winning text, reveals the chosen word"""
     yay = "You win! The word was {}!".format(chosen_word_string)
     print(yay)
     return yay
 
+
 def loss_text(chosen_word_string):
     """Prints the losing text, reveals the chosen word"""
     nay = "Sorry! You lose. The word was {}.".format(chosen_word_string)
     print(nay)
     return nay
+
 
 def play_again(end_input):
     if end_input == 'Y':
@@ -151,22 +198,31 @@ def play_again(end_input):
 
 if __name__ == "__main__":
     while True:
-        chosen_word = chooser(word_pull())
+
+        starting_text_a()
+        game_mode = starting_text_b()
+        if game_mode == 'E':
+            subset = easy_sorter(word_pull())
+            chosen_word = chooser(subset)
+        elif game_mode == 'M':
+            subset = med_sorter(word_pull())
+            chosen_word = chooser(subset)
+        elif game_mode == 'H':
+            subset = hard_sorter(word_pull())
+            chosen_word = chooser(subset)
+        else:
+            print("If you can't follow directions, you can't play.")
+            quit()
 
         chosen_word_string = set_chosen_word_string(chosen_word)
         chosen_word_list = set_chosen_word_list(chosen_word_string)
-        #print(chosen_word_list)
         chosen_word_length = length_checker(chosen_word)
         cwl = chosen_word_list[:]
-
-        #print(chosen_word_list)
-        #print(chosen_word_string)
 
         counter = 8
         guess_list = []
 
-        starting_text_a()
-        starting_text_b(chosen_word_length)
+        starting_text_c(chosen_word_length)
         list_visualizer(chosen_word_list, guess_list)
 
         while counter > 0:
@@ -177,25 +233,17 @@ if __name__ == "__main__":
             while repeat_guess_check(capsletter, guess_list) == "guess again":
                 capsletter = get_guess()
             guess_list = append_guess_list(capsletter, guess_list)
-            #print(chosen_word_string)
-            #print(chosen_word_list)
-            #print(guess_list)
             list_visualizer(chosen_word_list, guess_list)
             counter = guess_checker(capsletter, chosen_word_string, counter)
-            #print(counter)
-
-            #print(chosen_word_list)
-            #print(guess_list)
-
 
             x = win_check(chosen_word_list, guess_list)
-            if x == True:
+            if x is True:
                 victory_text(chosen_word_string)
                 break
 
         else:
             loss_text(chosen_word_string)
-        print ("""Would you like to play again?\n If so, type Y""")
+        print("""Would you like to play again?\n If so, type Y""")
         end_input = input("> ")
         end_input = end_input.upper()
         play_again(end_input)
--- test_file.py
+++ test_file.py
@@ -5,17 +5,36 @@ def test_chooser():
     lista = ['alpha', 'beta', 'gamma', 'delta']
     assert mw.chooser(lista) in lista
 
+
 def test_length_checker():
     assert mw.length_checker("alligator\n") == 9
+
+
+def test_easy_sorter():
+    lista = ['cat\n', 'apple\n', 'bananas\n', 'encyclopedias\n']
+    assert mw.easy_sorter(lista) == ['apple\n']
+
+
+def test_med_sorter():
+    lista = ['cat\n', 'apple\n', 'bananas\n', 'encyclopedias\n']
+    assert mw.med_sorter(lista) == ['bananas\n']
+
+
+def test_hard_sorter():
+    lista = ['cat\n', 'apple\n', 'bananas\n', 'encyclopedias\n']
+    assert mw.hard_sorter(lista) == ['encyclopedias\n']
+
 
 def test_guess_checker():
     assert mw.guess_checker('A', "APPLE", 8) == 8
     assert mw.guess_checker('B', "APPLE", 8) == 7
 
+
 def test_append_guess_list():
     guess = 'B'
     glist = ['D', 'C']
     assert mw.append_guess_list(guess, glist) == ['D', 'C', 'B']
+
 
 def test_list_visualizer():
     lista = ['a', 'p', 'p', 'l', 'e']
@@ -26,21 +45,27 @@ def test_list_visualizer():
     assert mw.list_visualizer(lista, listc) == 'a p p _ e'
     assert mw.list_visualizer(lista, []) == '_ _ _ _ _'
 
+
 def test_set_chosen_word_string():
     word = 'apPle\n'
     assert type(mw.set_chosen_word_string(word)) == str
     assert mw.set_chosen_word_string(word) == "APPLE"
+
 
 def test_set_chosen_word_list():
     word = 'APPLE'
     assert type(mw.set_chosen_word_list(word)) == list
     assert mw.set_chosen_word_list(word) == ['A', 'P', 'P', 'L', 'E']
 
-def test_starting_text_b():
-    assert mw.starting_text_b(5) == "I have chosen a word at random. It has 5 letters."
+
+def test_starting_text_c():
+    assert mw.starting_text_c(5) == (
+        "I have chosen a word at random. It has 5 letters.")
+
 
 def test_guess_text():
     assert mw.guess_text(8) == "Make a guess! You have 8 left."
+
 
 def test_repeat_guess_check():
     guessa = "A"
@@ -49,27 +74,32 @@ def test_repeat_guess_check():
     assert mw.repeat_guess_check(guessa, guess_list) == "pass"
     assert mw.repeat_guess_check(guessb, guess_list) == "guess again"
 
+
 def test_error_checking():
     assert mw.error_checking('1') == "not ok"
     assert mw.error_checking('!') == "not ok"
     assert mw.error_checking('ab') == "not ok"
     assert mw.error_checking('A') == "ok"
 
+
 def test_win_check():
     lista = ['A', 'P', 'P', 'L', 'E']
     listb = ['A', 'B', 'P', 'Q', 'E', 'L']
     listc = ['Q', 'P']
-    assert mw.win_check(lista, listb) == True
-    assert mw.win_check(lista, listc) == False
+    assert mw.win_check(lista, listb) is True
+    assert mw.win_check(lista, listc) is False
+
 
 def test_lose_check():
     counter1 = 1
     counter2 = 0
-    assert mw.lose_check(counter1) == False
-    assert mw.lose_check(counter2) == True
+    assert mw.lose_check(counter1) is False
+    assert mw.lose_check(counter2) is True
+
 
 def test_victory_text():
     assert mw.victory_text("APPLE") == "You win! The word was APPLE!"
+
 
 def test_loss_text():
     assert mw.loss_text("APPLE") == "Sorry! You lose. The word was APPLE."
